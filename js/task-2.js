574bc489644cf768e3db2b51b435b50377a637d5
Serhii Mordvaniuk <sm636332@gmail.com>
2024-09-25 09:02:24 +0300

    done

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,4 @@
 "use strict";
-//  Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
-
-// Оголоси наступні методи класу:
-
-// getItems() — повертає масив поточних товарів у приватній властивості items.
-// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
-// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
 
 class Storage {
   #items = [];
@@ -28,12 +21,7 @@ class Storage {
 //
 //
 //
-const storage = new Storage([
-  "Nanitoids",
-  "Prolonger",
-  "Antigravitator",
-  "lolo",
-]);
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
 storage.addItem("Droid");
